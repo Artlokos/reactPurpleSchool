2237376306a71e642ca3190caa9a5b38b0135680
Artlokos <artlokos@yandex.ru>
2024-10-23 02:40:52 +1000

    firstStep dz1_toReview

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 import './App.css';
 import Button from './components/Button/Button';
-import CardButton from './components/CardButton/CardButton';
-import JournalItem from './components/JournalItem/JournalItem';
-
+import Header from './components/Header/Header';
+import Paragraph from './components/Paragraph/Paragraph';
 function App() {
-  // const [count, setCount] = useState(0);
-  const data = [
-    {
-      title:'Подготовка к обновлению курсов',
-      date: new Date(),
-      text: 'Горные походы'
-    },
-    {
-      title:'Поход в горы',
-      date: new Date(),
-      text: 'Фалаза'
-    }
-  ];
+
   return (
     <>
-     <h1>Заголовок</h1>
-     <p>Первый компонент</p>
-     <Button/>
-    <CardButton>
-     + Новое воспоминание
-    </CardButton>
-
-     <CardButton>
-     <JournalItem 
-        title={data[0].title}
-        text={data[0].text}
-        date={data[0].date}
-     />
-     </CardButton>
-     <CardButton>
-      <JournalItem 
-        title={data[1].title}
-        text={data[1].text}
-        date={data[1].date}
-     />
-     </CardButton>
+      <Header value="Поиск"/>
+      <Paragraph value="Введите название фильма, сериала или мультфильма для поиска и добавления в избранное."/>
+      <Button value="Искать"/>
+      
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.css';
+
+function Header({value}){
+    return (       
+            <h1 className='header-1'>{value}</h1>  
+            );
+}
+export default Header;
